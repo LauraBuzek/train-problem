cf9b52274fe136e07d1410014d21bb3973a1b084
Laura Buzek <lbuzek@gmail.com>
2017-04-05 17:54:54 -0600

    working on running program:

--- src/com/run/Trains.java
+++ src/com/run/Trains.java
@@ -5,20 +5,29 @@ import com.graph.TownGraph;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Trains {
     public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter file path for the desired input graph:");
+        String filename = scanner.next();
         FileReader input = null;
         BufferedReader bufferedInput = null;
 
         try {
-            input = new FileReader("src/resource/input_graph.txt");
+            //"src/com/resource/input_graph.txt"
+            input = new FileReader(filename);
             bufferedInput = new BufferedReader(input);
 
             TownGraph townGraph = TownGraphGenerator.createTownGraph(bufferedInput);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        while(true) {
+            System.out.println("Request ")
     }
 
 
